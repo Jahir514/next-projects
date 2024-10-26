83ee8daa011b4d8edbbafdbe28d2a043e0b206fd
Jahir514 <jahirhasan.amana@gmail.com>
2024-10-26 20:41:25 +0600

    google analytics added

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,30 +1,42 @@
-import { JetBrains_Mono } from 'next/font/google'
-import './globals.css'
+import { JetBrains_Mono } from "next/font/google";
+import "./globals.css";
 // components
-import Header from '@/components/Header'
-import PageTransition from '@/components/PageTransition'
-import StairTransition from '@/components/StairTransition'
+import Header from "@/components/Header";
+import PageTransition from "@/components/PageTransition";
+import StairTransition from "@/components/StairTransition";
+import Script from "next/script";
 const jetbrainsMono = JetBrains_Mono({
-  subsets: ['latin'],
-  weight: ['100', '200', '300', '400', '500', '600', '700', '800'],
-  variable: '--font-jetbrainsMono',
-})
+  subsets: ["latin"],
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800"],
+  variable: "--font-jetbrainsMono",
+});
 
 export const metadata = {
-  title: 'Personal Portfolio',
-  description: 'website for my personal informatin and achievement',
-}
+  title: "Personal Portfolio",
+  description: "website for my personal informatin and achievement",
+};
 
 export default function RootLayout({ children }) {
   return (
-    <html lang='en'>
+    <html lang="en">
       <body
         className={`${jetbrainsMono.variable} ${jetbrainsMono.variable} antialiased`}
       >
+        <Script
+          async
+          src={`https://www.googletagmanager.com/gtag/js?id=G-B6HPNNF2SQ`}
+        />
+        <Script>
+          {`window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'G-B6HPNNF2SQ');`}
+        </Script>
         <Header />
         <StairTransition />
         <PageTransition>{children}</PageTransition>
       </body>
     </html>
-  )
+  );
 }
